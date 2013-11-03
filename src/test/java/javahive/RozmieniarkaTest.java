58e6054978e88772ba9357559ee1681cc261f604
Mateusz-Sajnog <mat.sajnog@gmail.com>
2013-11-04 00:13:17 +0100

    Dodanie kilku testow

--- src/test/java/javahive/RozmieniarkaTest.java
+++ src/test/java/javahive/RozmieniarkaTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class RozmieniarkaTest {
 	private final double[] NOMINALY_PL = { 200, 100, 50, 20, 10, 5, 2, 1, .50, .20, .10, .05, .02, 0.01 };
 	@Test
-	public void PowinienRozmieniacPoprawnie124(){
+	public void t001PowinienRozmieniacPoprawnie124(){
 		//given
 		Rozmieniarka rozmieniarka=new RozmieniarkaMSImpl();
 		//when
@@ -17,69 +17,51 @@ public class RozmieniarkaTest {
 		assertTrue(nominaly.getIlosc(2)==2);
 	}
 	@Test
-	public void PowinienRozmieniacPoprawnie125_11(){
+	public void t002PowinienRozmieniacPoprawnie555_55(){
 		//given
 		Rozmieniarka rozmieniarka=new RozmieniarkaMSImpl();
 		//when
-		Nominaly nominaly=rozmieniarka.rozmien(125.11, NOMINALY_PL);
+		Nominaly nominaly=rozmieniarka.rozmien(555.55, NOMINALY_PL);
 		//then
+		assertTrue(nominaly.getIlosc(200)==2);
 		assertTrue(nominaly.getIlosc(100)==1);
-		assertTrue(nominaly.getIlosc(20)==1);
+		assertTrue(nominaly.getIlosc(50)==1);
 		assertTrue(nominaly.getIlosc(5)==1);
-	    assertTrue(nominaly.getIlosc(.10) == 1);
-	    assertTrue(nominaly.getIlosc(.01) == 1);
+	    assertTrue(nominaly.getIlosc(.50) == 1);
+	    assertTrue(nominaly.getIlosc(.05) == 1);
 	}
 
 	
 	@Test(expected = ArithmeticException.class) 
-	public void ArgumentyUjemnePowinnyWyrzucaWyjatek()	{
+	public void t003ArgumentyUjemnePowinnyWyrzucaWyjatek()	{
 		Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
 		Nominaly nominaly = rozmieniarka.rozmien(-1, NOMINALY_PL);
 		fail();
 		
 	}
-	public void PowinienRozmieniacPoprawnie218_76() {
+	public void t004PowinienRozmieniacPoprawnie313_49() {
         // given
         Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
         // when
-        Nominaly nominaly = rozmieniarka.rozmien(218.76, NOMINALY_PL);
+        Nominaly nominaly = rozmieniarka.rozmien(313.49, NOMINALY_PL);
         // then
         assertTrue(nominaly.getIlosc(200) == 1);
         assertTrue(nominaly.getIlosc(10) == 1);
-        assertTrue(nominaly.getIlosc(5) == 1);
+        assertTrue(nominaly.getIlosc(10) == 1);
         assertTrue(nominaly.getIlosc(2) == 1);
         assertTrue(nominaly.getIlosc(1) == 1);
-        assertTrue(nominaly.getIlosc(.50) == 1);
-        assertTrue(nominaly.getIlosc(.20) == 1);
+        assertTrue(nominaly.getIlosc(.20) == 2);
         assertTrue(nominaly.getIlosc(.05) == 1);
-        assertTrue(nominaly.getIlosc(.01) == 1);
+        assertTrue(nominaly.getIlosc(.02) == 2);
 }
 @Test
-public void PowinienRozmieniacPoprawnie591_83() {
-        // given
-        Rozmieniarka rozmieniarka =new RozmieniarkaMSImpl();
-        // when
-        Nominaly nominaly = rozmieniarka.rozmien(591.83, NOMINALY_PL);
-        // them
-        assertTrue(nominaly.getIlosc(200) == 2);
-        assertTrue(nominaly.getIlosc(100) == 1);
-        assertTrue(nominaly.getIlosc(50) == 1);
-        assertTrue(nominaly.getIlosc(20) == 2);
-        assertTrue(nominaly.getIlosc(1) == 1);
-        assertTrue(nominaly.getIlosc(.50) == 1);
-        assertTrue(nominaly.getIlosc(.20) == 1);
-        assertTrue(nominaly.getIlosc(.10) == 1);
-        assertTrue(nominaly.getIlosc(.02) == 1);
-        assertTrue(nominaly.getIlosc(.01) == 1);
-}
-@Test
-public void PowinienRozmieniacPoprawnie388_88() {
+public void t005PowinienRozmieniacPoprawnie588_88() {
         // given
         Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
         // when
-        Nominaly nominaly = rozmieniarka.rozmien(388.88, NOMINALY_PL);
+        Nominaly nominaly = rozmieniarka.rozmien(588.88, NOMINALY_PL);
         // then
-        assertTrue(nominaly.getIlosc(200) == 1);
+        assertTrue(nominaly.getIlosc(200) == 2);
         assertTrue(nominaly.getIlosc(100) == 1);
         assertTrue(nominaly.getIlosc(50) == 1);
         assertTrue(nominaly.getIlosc(20) == 1);
@@ -94,18 +76,52 @@ public void PowinienRozmieniacPoprawnie388_88() {
         assertTrue(nominaly.getIlosc(0.02) == 1);
         assertTrue(nominaly.getIlosc(0.01) == 1);
 }
-@Test (expected = ArithmeticException.class)
-public void PowinienRzucacWyjatekWPrzypadkuUjemnejKwoty() {
+
+@Test
+public void t006PowinienPoprawnieRozmieniac0_01() {
         // given
         Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
         // when
-        final double kwota = -100.0;
-        Nominaly nominaly = rozmieniarka.rozmien(kwota, NOMINALY_PL);
+        Nominaly nominaly = rozmieniarka.rozmien(0.01, NOMINALY_PL);
         // then
-        fail();
+        assertTrue(nominaly.getIlosc(0.01) == 1);
 }
 @Test
-public void PowinienPoprawnieRozmieniac1111_11() {
+public void t007PowinienPoprawnieRozmieniac0_11() {
+        // given
+        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
+        // when
+        Nominaly nominaly = rozmieniarka.rozmien(0.11, NOMINALY_PL);
+        // then
+        assertTrue(nominaly.getIlosc(0.10) == 1);
+        assertTrue(nominaly.getIlosc(0.01) == 1);
+}
+@Test
+public void t008PowinienPoprawnieRozmieniac1_11() {
+        // given
+        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
+        // when
+        Nominaly nominaly = rozmieniarka.rozmien(1.11, NOMINALY_PL);
+        // then
+        assertTrue(nominaly.getIlosc(1) == 1);
+        assertTrue(nominaly.getIlosc(0.10) == 1);
+        assertTrue(nominaly.getIlosc(0.01) == 1);
+}
+@Test
+public void t009PowinienPoprawnieRozmieniac111_11() {
+        // given
+        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
+        // when
+        Nominaly nominaly = rozmieniarka.rozmien(111.11, NOMINALY_PL);
+        // then
+        assertTrue(nominaly.getIlosc(100) == 1);
+        assertTrue(nominaly.getIlosc(10) == 1);
+        assertTrue(nominaly.getIlosc(1) == 1);
+        assertTrue(nominaly.getIlosc(0.10) == 1);
+        assertTrue(nominaly.getIlosc(0.01) == 1);
+}
+@Test
+public void t010PowinienPoprawnieRozmieniac1111_11() {
         // given
         Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
         // when
@@ -119,40 +135,7 @@ public void PowinienPoprawnieRozmieniac1111_11() {
         assertTrue(nominaly.getIlosc(0.01) == 1);
 }
 @Test
-public void PowinienPoprawnieRozmieniac0_01() {
-        // given
-        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
-        // when
-        Nominaly nominaly = rozmieniarka.rozmien(0.01, NOMINALY_PL);
-        // then
-        assertTrue(nominaly.getIlosc(0.01) == 1);
-}
-@Test
-public void PowinienPoprawnieRozmieniac1_11() {
-        // given
-        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
-        // when
-        Nominaly nominaly = rozmieniarka.rozmien(1.11, NOMINALY_PL);
-        // then
-        assertTrue(nominaly.getIlosc(1) == 1);
-        assertTrue(nominaly.getIlosc(0.10) == 1);
-        assertTrue(nominaly.getIlosc(0.01) == 1);
-}
-@Test
-public void PowinienPoprawnieRozmieniac111_11() {
-        // given
-        Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
-        // when
-        Nominaly nominaly = rozmieniarka.rozmien(111.11, NOMINALY_PL);
-        // then
-        assertTrue(nominaly.getIlosc(100) == 1);
-        assertTrue(nominaly.getIlosc(10) == 1);
-        assertTrue(nominaly.getIlosc(1) == 1);
-        assertTrue(nominaly.getIlosc(0.10) == 1);
-        assertTrue(nominaly.getIlosc(0.01) == 1);
-}
-@Test
-public void PowinienPoprawnieRozmieniac11111_11() {
+public void t011PowinienPoprawnieRozmieniac11111_11() {
         // given
         Rozmieniarka rozmieniarka = new RozmieniarkaMSImpl();
         // when
